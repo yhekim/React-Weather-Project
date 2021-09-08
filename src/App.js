860a36725dcdcd8b7dd14a448054e46babb179ba
yhekim <yusufhekimyh@gmail.com>
2021-09-08 12:39:15 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 //api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-import React,{useState,useEffect} from "react";
-import axios from 'axios';
-import { usePosition } from "use-position";
+import React from "react";
+
+
 import HavaDurumu from "./component/HavaDurumu";
 
 
 const App=()=> {
 
-  const [weather,setWeather]=useState({});
-
-const key=process.env.REACT_APP_WEATHER_KEY;
-
-const lang=navigator.language.split("-")[0];
 
 
-const {latitude,longitude}=usePosition();
 
-  const getWeatherData=(lat,lon)=>{
-     axios
-      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&lang=${lang}&units=metric`)
-    .then(response=>setWeather(response.data))
-     .catch(err=>console.log(err))
- }
+
+
+
+
+
 
 //   const getWeather=(lat,lon)=>{
 //     async const key=process.env.REACT_APP_WEATHER_KEY;
@@ -36,19 +29,14 @@ const {latitude,longitude}=usePosition();
       
 //     }
 
-useEffect(()=>{
 
-
-latitude && longitude && getWeatherData(latitude,longitude);
-
-  },[latitude,longitude])
 
 
 
   return (
     <div className="App">
       <h1>Hava Durumu</h1>
-      <HavaDurumu weather={weather}/>
+      <HavaDurumu />
     </div>
   );
 }
--- src/component/HavaDurumu.jsx
+++ src/component/HavaDurumu.jsx
@@ -1,18 +1,44 @@
-const HavaDurumu=(props)=>{
-    
-const {weather}=props;
-console.log(props.weather)
-    
+import React,{useEffect,useState} from "react";
+import { usePosition } from "use-position";
+import axios from "axios";
 
+const HavaDurumu=()=>{
+    
+const [weather,setWeather]=useState("");
+
+ const getWeatherData=(lat,lon)=>{
+     axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&lang=${lang}&units=metric`)
+    .then(response=>setWeather(response.data))
+.catch(err=>console.log(err))
+}
+
+
+
+const key=process.env.REACT_APP_WEATHER_KEY;
+
+const lang=navigator.language.split("-")[0];
+const {latitude,longitude}=usePosition();
+
+useEffect(()=>{
+
+
+    latitude && longitude && getWeatherData(latitude,longitude);
+    
+      },[latitude,longitude])
+
+if(weather===""){
+    return <h2>Loading...</h2>
+}
 
     return(
         <div>
-            <h2>Şehir Hava Durumu Bilgileri</h2>
+           <h2>Şehir Hava Durumu Bilgileri</h2>
             <br/>
               <h3>Şehir : {weather.name} </h3>
            <h4>Hava Durum Bilgisi : {weather.weather.map((data)=>data.description).join(",")}</h4>
             <h4>Sıcaklık : {weather.main.temp} C</h4>
-            <h4>Tarih : {new Date(weather.dt*1000).toLocaleDateString()}</h4>  
+            <h4>Tarih : {new Date(weather.dt*1000).toLocaleDateString()}</h4> 
         </div>
 
     )
